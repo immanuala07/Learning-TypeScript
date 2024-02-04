affcaf9ca4b9cfe411a080977ba7652649abaaad
Immanual <immanualatrinity@gmail.com>
2024-02-04 15:59:21 +0530

    Working with Numbers, Strings & Booleans

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    // 	throw new Error('Incorrect input!');
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        /*
+        Below statement considers all the operands as strings,
+        and gives wrong ouput for numerical calculation,
+        so we have to do numeric calculations above the below statement
+        */
+        // console.log(phrase + n1 + n2);
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
